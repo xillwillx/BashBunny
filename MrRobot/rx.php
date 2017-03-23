59749003b193bd7854431d9e556fea1d428c0e5d
illwill <illwill@illmob.org>
2017-03-23 00:27:19 -0400

    Delete rx.php

--- MrRobot/rx.php
+++
@@ -1,5 +0,0 @@
-<?php
-date_default_timezone_set('Europe/Rome');
-$file = $_SERVER['REMOTE_ADDR'] . "_" . date("Y-m-d_H-i-s") . ".txt";
-file_put_contents($file, file_get_contents("php://input"));
-?>
