215da406cea03a0a041c8341e78bdb3bf17c4b78
illwill <illwill@illmob.org>
2017-03-23 14:41:50 -0400

    Update server.py

--- MrRobot/server.py
+++ MrRobot/server.py
@@ -1,3 +1,5 @@
+
+
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 
 
@@ -6,33 +8,44 @@ IS_RUNNING = True
 
 class RequestServer(BaseHTTPRequestHandler):
     def _set_headers(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
+        self.send_response(200, "ok")
+        self.send_header('Content-type', 'text/plain')
+        self.protocol_version = 'HTTP/1.1'
 
     def do_GET(self):
-        try:    
+        self.send_response(200, "ok")
+        self.send_header("Content-type", "text/xml")
+        self.end_headers()
+        try:
             with open(self.path[1:], 'r+') as f:
                 data = f.read()
             self.wfile.write(data)
         except IOError:
             self.send_response(404)
             return
-        self._set_headers()
 
     def do_POST(self):
         global IS_RUNNING
 
-        if filename == 'EOF':
-            IS_RUNNING = False
-        else:
-            content_length = int(self.headers['Content-Length'])
-            data = self.rfile.read(content_length)
-            filename = self.path[1:]
-            with open("loot/{}.txt".format(filename), "w+") as f:
-                f.write(data)
+
+        self.send_response(200)
+        self.send_header('Access-Control-Allow-Credentials', 'true')
+        self.send_header('Access-Control-Allow-Origin', 'http://localhost:8888')
+        self.send_header("Content-type", "text/xml")
+        self.end_headers()
+        content_length = int(self.headers['Content-Length'])
+
+    #    if filename == 'EOF':
+    #        IS_RUNNING = False
+    #    else:
+
+
+        data = self.rfile.read(content_length)
+        filename = self.path[1:]
+        with open("loot/{}.txt".format(filename), "w+") as f:
+            f.write(data)
         self._set_headers()
-       
+
 
 def run(server_class=HTTPServer, handler_class=RequestServer, port=80):
     server_address = ('', port)
@@ -43,4 +56,3 @@ def run(server_class=HTTPServer, handler_class=RequestServer, port=80):
 
 
 if __name__ == '__main__':
-    run()
