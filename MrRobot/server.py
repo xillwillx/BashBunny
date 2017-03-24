1b46cac37ed102aa0430c00382bf87e9aa52fb17
illwill <illwill@illmob.org>
2017-03-24 05:57:57 -0400

    Update server.py

--- MrRobot/server.py
+++ MrRobot/server.py
@@ -1,10 +1,10 @@
-
-
+import os
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 
-
 IS_RUNNING = True
-
+abspath = os.path.abspath(__file__)
+CURR_DIR = os.path.dirname(abspath)
+os.chdir(CURR_DIR)
 
 class RequestServer(BaseHTTPRequestHandler):
     def _set_headers(self):
@@ -14,38 +14,39 @@ class RequestServer(BaseHTTPRequestHandler):
 
     def do_GET(self):
         self.send_response(200, "ok")
-        self.send_header("Content-type", "text/xml")
+        self.send_header("Content-type", "text/plain")
         self.end_headers()
         try:
-            with open(self.path[1:], 'r+') as f:
+            with open(CURR_DIR + self.path, 'r+') as f:
                 data = f.read()
             self.wfile.write(data)
         except IOError:
             self.send_response(404)
+            self.wfile.write(CURR_DIR)
             return
 
     def do_POST(self):
         global IS_RUNNING
 
-
         self.send_response(200)
-        self.send_header('Access-Control-Allow-Credentials', 'true')
-        self.send_header('Access-Control-Allow-Origin', 'http://localhost:8888')
-        self.send_header("Content-type", "text/xml")
+        self.send_header("Content-type", "text/plain")
         self.end_headers()
         content_length = int(self.headers['Content-Length'])
-
-    #    if filename == 'EOF':
-    #        IS_RUNNING = False
-    #    else:
-
-
-        data = self.rfile.read(content_length)
         filename = self.path[1:]
-        with open("loot/{}.txt".format(filename), "w+") as f:
-            f.write(data)
-        self._set_headers()
 
+        if filename == 'EOF':
+            data = self.rfile.read(content_length)
+            with open(CURR_DIR + "/loot/{}".format(filename), "w+") as f:
+                f.write(data)
+                f.close()
+                self.end_headers()
+                IS_RUNNING = False
+        else:
+            data = self.rfile.read(content_length)
+            with open(CURR_DIR + "/loot/{}.txt".format(filename), "w+") as f:
+                f.write(data)
+                f.close()
+        self._set_headers()
 
 def run(server_class=HTTPServer, handler_class=RequestServer, port=80):
     server_address = ('', port)
@@ -53,7 +54,6 @@ def run(server_class=HTTPServer, handler_class=RequestServer, port=80):
 
     while IS_RUNNING:
         httpd.handle_request()
-
 
 if __name__ == '__main__':
     run()
